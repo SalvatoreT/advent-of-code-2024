2001080e3cebf950043fe31c13a1102d7e452921
Salvatore Testa <sal@sal.dev>
2024-12-03 09:02:42 -0800

    Solve Day 3

---
+++ src/Day03.kt
@@ -0,0 +1,31 @@
+import java.nio.file.Path
+import kotlin.io.path.readLines
+
+fun day03part1(path: Path): Int =
+    path
+        .readLines()
+        .sumOf { line -> scores(line) }
+
+private fun scores(input: String): Int {
+    val regex = Regex("""mul\((?<first>[1-9]\d{0,2}),\s*(?<second>[1-9]\d{0,2})\)""")
+
+    return regex
+        .findAll(input)
+        .map { match ->
+            val first = match.groups["first"]?.value!!.toInt()
+            val second = match.groups["second"]?.value!!.toInt()
+            first * second
+        }.sum()
+}
+
+fun day03part2(path: Path): Int =
+    path
+        .readLines()
+        .joinToString()
+        .let { line -> removeOffCommands(line) }
+        .let { line -> scores(line) }
+
+private fun removeOffCommands(input: String): String {
+    val regex = Regex("""don't\(\).*?(do\(\)|$)""")
+    return input.replace(regex, "")
+}
---
+++ test/Day03KtTest.kt
@@ -0,0 +1,25 @@
+import kotlin.io.path.Path
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day03KtTest {
+    @Test
+    fun day03part1_example() {
+        assertEquals(161, day03part1(Path("resources/day03.example.txt")))
+    }
+
+    @Test
+    fun day03part1() {
+        println("Day 3 Part 1 is ${day03part1(Path("resources/day03.first.txt"))}")
+    }
+
+    @Test
+    fun day03part2_example() {
+        assertEquals(48, day03part2(Path("resources/day03.example2.txt")))
+    }
+
+    @Test
+    fun day03part2() {
+        println("Day 3 Part 2 is ${day03part2(Path("resources/day03.first.txt"))}")
+    }
+}
