3bf852eccc99dd35bb8375e00342dbb8340ce5a1
Salvatore Testa <sal@sal.dev>
2024-12-06 09:08:30 -0800

    Solve Day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -21,11 +21,11 @@ fun day06part1(path: Path): Int {
                         val tile =
                             when (symbol) {
                                 '.' -> Tile.Empty
-                                '^' -> Tile.Empty
+                                '^' -> Tile.Guard
                                 '#' -> Tile.Obstacle
                                 else -> throw Exception("Unexpected '$symbol' symbol")
                             }
-                        if (symbol == '^') {
+                        if (tile == Tile.Guard) {
                             visited += Pair(row, column)
                             currentPosition = Pair(row, column)
                         }
@@ -49,3 +49,77 @@ fun day06part1(path: Path): Int {
     }
     return visited.size
 }
+
+fun day06part2(path: Path): Int {
+    val visited = mutableSetOf<Pair<Int, Int>>()
+    var startingVelocity = Pair(-1, 0)
+    var startingPosition = Pair(0, 0)
+    val floor =
+        path
+            .readLines()
+            .mapIndexed { row, line ->
+                line
+                    .toCharArray()
+                    .mapIndexed { column, symbol ->
+                        val tile =
+                            when (symbol) {
+                                '.' -> Tile.Empty
+                                '^' -> Tile.Guard
+                                '#' -> Tile.Obstacle
+                                else -> throw Exception("Unexpected '$symbol' symbol")
+                            }
+                        if (symbol == '^') {
+                            startingPosition = Pair(row, column)
+                        }
+                        Pair(column, tile)
+                    }.toMap()
+                    .let { Pair(row, it) }
+            }.toMap()
+    return floor
+        .map { (row, rowMap) ->
+            rowMap.count { (column, tile) ->
+                if (tile == Tile.Empty) {
+                    val newRow = rowMap.toMutableMap().also { it[column] = Tile.Obstacle }
+                    val newFloor = floor.toMutableMap().also { it[row] = newRow }
+                    infiniteLoops(newFloor, startingPosition, startingVelocity)
+                } else {
+                    false
+                }
+            }
+        }.sum()
+}
+
+private fun infiniteLoops(
+    floor: Map<Int, Map<Int, Tile>>,
+    startingPosition: Pair<Int, Int>,
+    startingVelocity: Pair<Int, Int>,
+): Boolean {
+    var position = startingPosition
+    var velocity = startingVelocity
+    var visited = mutableListOf<Pair<Int, Int>>()
+    while (floor[position.first]?.contains(position.second) == true) {
+        visited += position
+        if (visited.size > 16900) {
+            val (lastLeft, lastRight) = visited.takeLast(2)
+            val occurrences =
+                visited
+                    .windowed(2)
+                    .count { (sampleLeft, sampleRight) -> sampleLeft == lastLeft && sampleRight == lastRight }
+            return occurrences > 1
+        }
+        while (floor[position.first + velocity.first]?.get(position.second + velocity.second) == Tile.Obstacle) {
+            velocity = nextVelocity(velocity)
+        }
+        position = Pair(position.first + velocity.first, position.second + velocity.second)
+    }
+    return false
+}
+
+private fun nextVelocity(velocity: Pair<Int, Int>): Pair<Int, Int> =
+    when (velocity) {
+        Pair(-1, 0) -> Pair(0, 1)
+        Pair(0, 1) -> Pair(1, 0)
+        Pair(1, 0) -> Pair(0, -1)
+        Pair(0, -1) -> Pair(-1, 0)
+        else -> throw Exception("Unexpected velocity $velocity")
+    }
--- test/Day06KtTest.kt
+++ test/Day06KtTest.kt
@@ -12,4 +12,14 @@ class Day06KtTest {
     fun day06part1() {
         println("Day 06 Part 1 is ${day06part1(Path("resources/day06.input.txt"))}")
     }
+
+    @Test
+    fun day06part2_example() {
+        assertEquals(6, day06part2(Path("resources/day06.example.txt")))
+    }
+
+    @Test
+    fun day06part2() {
+        println("Day 06 Part 2 is ${day06part2(Path("resources/day06.input.txt"))}")
+    }
 }
